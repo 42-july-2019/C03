6fe9d5a2b7ee3c621423b2275f63af2f2b33ae6a
Nathanael LABREUIL <alabreui@e2r12p23.42.fr>
2019-07-09 14:24:31 +0200

    Add ex03

---
+++ ex03/ft_strncat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/09 14:12:59 by alabreui          #+#    #+#             */
+/*   Updated: 2019/07/09 14:23:09 by alabreui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int length;
+	unsigned int i;
+
+	length = 0;
+	i = 0;
+	while (dest[length] != '\0')
+		length++;
+	while (i < nb && src[i] != '\0')
+	{
+		dest[length + i] = src[i];
+		i++;
+	}
+	dest[length + i] = '\0';
+	return (dest);
+}
